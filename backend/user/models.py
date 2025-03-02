4ec73ecc3c60f8f6d597905544c12b5049a630a1
pradosh_mihir_dash <pradoshmihirdash1928@gmail.com>
2025-03-02 21:42:57 +0530

    Update models.py

--- backend/user/models.py
+++ backend/user/models.py
@@ -51,3 +51,12 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+
+# User profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(blank=True)
+    # avatar = models.ImageField(upload_to='avatars/', default='default.jpg')
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
